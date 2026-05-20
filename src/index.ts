d61152c15f02155955e35d57e0f5731309d7798d
ancyloce <ancyloce@gmail.com>
2026-05-20 14:28:04 -0700

    📦 chore: add zustand dependency and export history store functionality in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export { CanvasStudio } from "./CanvasStudio.js";
 export type { CanvasStudioProps } from "./CanvasStudio.js";
+export {
+	createHistoryStore,
+	DEFAULT_HISTORY_LIMIT,
+} from "./stores/history-store.js";
+export type {
+	CreateHistoryStoreOptions,
+	HistoryState,
+	HistoryStoreApi,
+} from "./stores/history-store.js";
